60c8890a3af2e25b06bfb72989073df76b72fa88
Fred McWilliams <fredmcwilliams92@gmail.com>
2021-04-17 20:07:06 -0500

    gave questions class names, set up css and added in a timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,12 +46,22 @@ var quizQuestions = [{
 },
 ];
 
-// hide options from main page
-// var hideButtons = document.getElementById('quiz-page');
-// hideButtons.remove();
-
-// start quiz
+// start quiz & timer
 startQuizButton.addEventListener('click', startTimedQuiz);
+startQuizButton.addEventListener('click', beginTimer);
+
+// timer
+function beginTimer(){
+  let timeLeft = 200
+  var countdownTimer = document.getElementById('timeLeft');
+  setInterval(function() {
+    if(timeLeft <= 0) {
+      clearInterval(timeLeft = 0)
+    }
+    countdownTimer.innerHTML = timeLeft
+    timeLeft -=1
+  }, 1000)
+};
 
 // start quiz on click
 function startTimedQuiz() {
@@ -59,56 +69,59 @@ function startTimedQuiz() {
   instructions.remove();
 
   // create element for question 1 and append questions to said element
-  var questionOne = document.createElement('div');
-  questionOne.textContent = quizQuestions[0].question
+  var quizQuestion = document.createElement('div');
+  quizQuestion.className = "questionCSS";
+  quizQuestion.textContent = quizQuestions[0].question
   var houseQuestions = document.getElementById('houseQuestions');
-  houseQuestions.appendChild(questionOne);
+  houseQuestions.appendChild(quizQuestion);
 
   // create a unordered list to house answers
   var mainQuestion = document.createElement('ul');
+  mainQuestion.className = 'ulCSS'
 
   // create element for first answer option and append answer to said element
-  var answerOneListItem = document.createElement('li');
+  var answerOne = document.createElement('li');
   var answerOneButton = document.createElement('button');
+  answerOneButton.className = 'buttonCSS'
   answerOneButton.textContent = quizQuestions[0].choiceA;
-  answerOneListItem.appendChild(answerOneButton);
+  answerOne.appendChild(answerOneButton);
   var questionAnswerOne = document.getElementById('houseQuestions');
-  mainQuestion.appendChild(answerOneListItem)
+  mainQuestion.appendChild(answerOne)
   questionAnswerOne.appendChild(mainQuestion);
 
   // create element for the second answer option and append answer to said element
   var answerTwo = document.createElement('li');
-  answerTwo.textContent = quizQuestions[0].choiceB
+  var answerTwoButton = document.createElement('button');
+  answerTwoButton.className = 'buttonCSS'
+  answerTwoButton.textContent = quizQuestions[0].choiceB
+  answerTwo.appendChild(answerTwoButton);
   var questionAnswerTwo = document.getElementById('houseQuestions');
-  questionAnswerTwo.appendChild(answerTwo);
+  mainQuestion.appendChild(answerTwo);
+  questionAnswerTwo.appendChild(mainQuestion);
 
   // create element for the third answer option and append answer to said element
   var answerThree = document.createElement('li');
-  answerThree.textContent = quizQuestions[0].choiceC
+  var answerThreeButton = document.createElement('button');
+  answerThreeButton.className = 'buttonCSS'
+  answerThreeButton.textContent = quizQuestions[0].choiceC
+  answerThree.appendChild(answerThreeButton);
   var questionAnswerThree = document.getElementById('houseQuestions');
-  questionAnswerThree.appendChild(answerThree);
+  mainQuestion.appendChild(answerThree);
+  questionAnswerThree.appendChild(mainQuestion);
 
   // create element for the fourth answer option and append answer to said element
   var answerFour = document.createElement('li');
-  answerFour.textContent = quizQuestions[0].choiceD
+  var answerFourButton = document.createElement('button');
+  answerFourButton.className = 'buttonCSS'
+  answerFourButton.textContent = quizQuestions[0].choiceD
+  answerFour.appendChild(answerFourButton);
   var questionAnswerFour = document.getElementById('houseQuestions');
-  questionAnswerFour.appendChild(answerFour);
-
-  // AFter appending all list items to the unordered list, append the unordered list to the houseQuestions div
+  mainQuestion.appendChild(answerFour);
+  questionAnswerFour.appendChild(mainQuestion);
 };
 
-function buildQuiz() {
-
-};
-
-function showResults() {
-
-};
+// on submit, move to next question and show if prior selection was correct/incorrect
+document.addEventListener('click', choiceA)
 
 
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show the result
 // submitButton.addEventListener('click', showResults);
