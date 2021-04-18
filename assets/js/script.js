cd0a91f6fbb6568c158a2289cb5d7eedaed3e103
Fred McWilliams <fredmcwilliams92@gmail.com>
2021-04-18 18:56:54 -0500

    working on function to house responses and confirm they are correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,12 +145,22 @@ function loopOnClick() {
   }
 };
 
-// check answer for correct/incorrect
+// collect answer and confirm if its correct
+function checkAnswer() {
+  var correctAnswerOne = quizQuestions.correctAnswer
+  console.log('correctAnswerOne');
+  var correctAnswerTwo = quizQuestions[1].correctAnswer
+  var correctAnswerThree = quizQuestions[2].correctAnswer
+  var correctAnswerFour = quizQuestions[3].correctAnswer
+  var correctAnswerFive= quizQuestions[4].correctAnswer
+}
 
-// end quiz
-function endQuiz() {
-  var showScores
-  if(loopQuestions === 5) {
-    alert('You have completed your quiz!')
-  }
-};
+// // end quiz
+// function endQuiz() {
+//   if(loopQuestions >= 4) {
+//     alert('You have completed your quiz!')
+//   }
+//   var showScores = document.getElementById('#quizScores');
+// };
+
+// post 
